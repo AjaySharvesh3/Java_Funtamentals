c21b2a15fa8bd2946c9c2510a8110955051a17d9
AjaySharvesh3 <ajaysharvesh.mp@gmail.com>
2018-01-06 04:35:37 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        system();
+        variables();
+    }
+    public static void system() {
+        System.out.println("Hello Java :) " );
+    }
+    public static void variables() {
+        int var = 100;
+        System.out.println("Initial var: " + var);
+        int value = 300;
+        System.out.println("Initial value: " + value);
+        value = var;
+        System.out.println("Updated variable value: " + value);
+        var = 200;
+        System.out.println("Updated variable var: " + var);
+        value = var;
+        System.out.println("Again Updated variable value: " + value);
+    }
+    /** Type    Size    MinValue    MaxValue    LiteralFormat
+     * byte     8       -128        127         0
+     * short    16      -32768      32767       0
+     * int      32      -2147483648 2147483647  0
+     * long     64      -9223372036 9223372036  0L
+     *                854775808     854775807
+      */
+}
